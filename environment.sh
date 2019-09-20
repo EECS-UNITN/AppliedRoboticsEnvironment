fdc68f5a999be65ef9690c6ff902a2e6af92c572
Valerio Magnago <valerio.magnago@hotmail.it>
2019-09-20 15:52:00 +0200

    chore: added bash alias and script to load the environment

---
+++ environment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CATKIN_SHELL=bash
+
+printf "* RoboticsEnvironment/environment.sh\n"
+if [ -z "$AR_CATKIN_ROOT" ]
+then
+  export AR_CATKIN_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+  source ${AR_CATKIN_ROOT}/aliases
+
+  # Display Applied Robotics logo
+  AR_logo
+
+  # check whether devel folder exists
+  printf "\t* Loading ROS environment\n"
+  if [ -f "${AR_CATKIN_ROOT}/devel/setup.bash" ]; then
+      # source setup.sh from same directory as this file
+      source "${AR_CATKIN_ROOT}/devel/setup.bash"
+      printf "\t\t* \e[32m DONE!\n\e[0m"
+  else
+      source "/opt/ros/kinetic/setup.bash"
+      printf "\t\e[31mYou need to build first before you can source\n\e[0m"
+      printf "\t\e[33mRun 'catkin build' in ${AR_CATKIN_ROOT} directory\n\e[0m"
+      read -p "\t\tWant to build it now? [y/n]" -n 1 -r
+      if [[ $REPLY =~ ^[Yy]$ ]]
+      then
+        AR_catkin_clean
+        AR_catkin_build
+        source "${AR_CATKIN_ROOT}/devel/setup.bash"
+      fi
+  fi
+  printf "\t* Exporting lib folder in LD_LIBRARY_PATH\n"
+  LD_LIBRARY_PATH="${AR_CATKIN_ROOT}/build:${LD_LIBRARY_PATH}"
+else
+  printf "\t*\e[33m Seems that you already source this environment.\e[0m\n"  
+fi
